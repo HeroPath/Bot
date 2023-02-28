89f7f66cbe026907ad67e4e02ebeb4739cd41f14
Giancarlo <g.galvarini@alumno.um.edu.ar>
2023-02-28 11:04:37 -0300

    add dotenv

--- app/npc/Attack.go
+++ app/npc/Attack.go
@@ -1,5 +1,43 @@
 package npc
 
-func AttackNPC() {
+import (
+	"aoweb-bot/app/models"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+func getListNpcs(ApiUrl string, token string) []models.Npc {
+	var npcList []models.Npc
+	req, _ := http.NewRequest("GET", ApiUrl+"npcs/zone/forest", bytes.NewBuffer([]byte("")))
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+	decoder.Decode(&npcList)
+	return npcList
+}
+
+func AttackNPC(ApiUrl string, users []models.User) []models.User {
+	for i := 0; i < len(users); i++ {
+		var npcList = getListNpcs(ApiUrl, users[i].Token)
+		data := map[string]interface{}{
+			"name": npcList[0].Name,
+		}
+
+		jsonData, _ := json.Marshal(data)
+
+		req, _ := http.NewRequest("POST", ApiUrl+"users/attack-npc", bytes.NewBuffer(jsonData))
+		req.Header.Set("Authorization", "Bearer "+users[i].Token)
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+		fmt.Println(resp.Status)
+	}
+	return users
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"aoweb-bot/app/models"
+	"aoweb-bot/app/npc"
 	"aoweb-bot/app/users"
 	"github.com/joho/godotenv"
 	"log"
@@ -19,10 +20,16 @@ func main() {
 	ApiUrl := os.Getenv("API_URL")
 	amountUsers, err := strconv.Atoi(os.Getenv("AMOUNT_USERS_TO_REGISTER"))
 	addInitialStatsRandom, err := strconv.ParseBool(os.Getenv("ADD_INITIAL_STATS_RANDOM"))
+	attackNpcs, err := strconv.ParseBool(os.Getenv("ATTACK_NPCS"))
 
 	var usersList []models.User
 	usersList = users.Register(ApiUrl, usersList, amountUsers)
 	time.Sleep(3 * time.Second)
 	usersList = users.Login(ApiUrl, usersList)
 	users.AddStats(ApiUrl, usersList, addInitialStatsRandom)
+
+	if attackNpcs {
+		npc.AttackNPC(ApiUrl, usersList)
+	}
+
 }
